b2910c9056ff00583a7c95488da8802164089403
Mathias Arlaud <mathias.arlaud@gmail.com>
2023-12-15 17:20:54 +0100

    cmp and snip

---
+++ lua/mtarld/plugins/cmp.lua
@@ -0,0 +1,103 @@
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    {"hrsh7th/cmp-nvim-lsp"},
+    {"hrsh7th/cmp-nvim-lsp-signature-help"},
+    {"onsails/lspkind.nvim"},
+    {"saadparwaiz1/cmp_luasnip"},
+    {"L3MON4D3/LuaSnip"},
+    {"rafamadriz/friendly-snippets"},
+    {"hrsh7th/cmp-buffer"},
+    {"hrsh7th/cmp-cmdline"},
+  },
+  event = { "BufEnter" },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
+
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    cmp.setup({
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "nvim_lsp_signature_help" },
+        { name = "luasnip" },
+        { name = "buffer" },
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Insert,
+          select = true,
+        },
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end
+      },
+      window = {
+        completion = cmp.config.window.bordered({
+          winhighlight = "Normal:Normal,FloatBorder:CmpItemAbbr,CursorLine:PmenuSel,Search:None",
+        }),
+        documentation = cmp.config.window.bordered({
+          winhighlight = "Normal:Normal,FloatBorder:CmpItemAbbr,CursorLine:PmenuSel,Search:None",
+        }),
+      },
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol_text",
+          menu = ({
+            buffer = "[Buffer]",
+            nvim_lsp = "[LSP]",
+            luasnip = "[LuaSnip]",
+            nvim_lua = "[Lua]",
+            latex_symbols = "[Latex]",
+          })
+        }),
+      },
+    })
+
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+          { name = "cmdline" }
+        }),
+    })
+  end,
+}
--- lua/mtarld/plugins/lsp-zero.lua
+++ lua/mtarld/plugins/lsp-zero.lua
@@ -1,76 +1,51 @@
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    dependencies = {
-        {'neovim/nvim-lspconfig'},
-        {'williamboman/mason.nvim'},
-        {'williamboman/mason-lspconfig.nvim'},
-        {'hrsh7th/nvim-cmp'},
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'L3MON4D3/LuaSnip'},
-    },
-    event = { "BufEnter" },
-    config = function()
-        local lsp = require('lsp-zero')
+  "VonHeikemen/lsp-zero.nvim",
+  dependencies = {
+    {"neovim/nvim-lspconfig"},
+    {"williamboman/mason.nvim"},
+    {"williamboman/mason-lspconfig.nvim"},
+    {"hrsh7th/cmp-nvim-lsp"},
+  },
+  event = { "BufEnter" },
+  config = function()
+    local lsp = require("lsp-zero")
+    local lspconfig = require("lspconfig")
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
 
-        lsp.on_attach(function(_, bufnr)
-            lsp.default_keymaps({buffer = bufnr})
-        end)
+    lsp.on_attach(function(_, bufnr)
+      lsp.default_keymaps({buffer = bufnr})
+    end)
 
-        require('mason').setup({})
-        require('mason-lspconfig').setup({
-          ensure_installed = {
-            "lua_ls",
-          },
-          handlers = {
-            lsp.default_setup,
-          },
-        })
+    mason.setup({})
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "lua_ls",
+      },
+      handlers = {
+        lsp.default_setup,
+      },
+    })
 
-        require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-        require('lspconfig').phpactor.setup({})
+    lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+    lspconfig.phpactor.setup({
+      require("cmp_nvim_lsp").default_capabilities()
+    })
 
-        lsp.setup()
-
-        local cmp = require('cmp')
-        cmp.setup({
-            mapping = cmp.mapping.preset.insert({
-                ["<CR>"] = cmp.mapping.confirm {
-                    behavior = cmp.ConfirmBehavior.Insert,
-                    select = true,
-                },
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif require("luasnip").expand_or_jumpable() then
-                        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-                    else
-                        fallback()
-                    end
-                end, { "i", "s", }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif require("luasnip").jumpable(-1) then
-                        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-            }),
-        })
-    end,
-    keys = {
-      {"<leader>ca", function()
-        vim.lsp.buf.code_action()
-      end},
-      {"<leader>ca", function()
-        vim.lsp.buf.code_action()
-      end, mode="v"},
-      {"K", function()
-        vim.lsp.buf.hover()
-      end},
-      {"<leader>R", function ()
-        vim.lsp.buf.rename()
-      end}
-    },
+    lsp.setup()
+  end,
+  keys = {
+    {"<leader>ca", function()
+      vim.lsp.buf.code_action()
+    end},
+    {"<leader>ca", function()
+      vim.lsp.buf.code_action()
+    end, mode="v"},
+    {"K", function()
+      vim.lsp.buf.hover()
+    end},
+    {"<leader>R", function ()
+      vim.lsp.buf.rename()
+    end}
+  },
 }
