624f41ed0e35d3a3956c2cf04dc570f25a14a250
Mathias Arlaud <mathias.arlaud@gmail.com>
2024-02-01 14:07:40 +0100

    replace dirbuf by oil

---
+++ lua/mtarld/plugins/oil.lua
@@ -0,0 +1,51 @@
+vim.keymap.set("n", "-", ":Oil<CR>")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "oil",
+  callback = function()
+
+    -- map ">" to live grep in current directory
+    vim.keymap.set("n", ">", function()
+      local oil = require("oil")
+
+      local entry = oil.get_cursor_entry()
+      local dir = oil.get_current_dir()
+      if not entry or not dir then
+        return
+      end
+
+      local path = dir .. entry.name
+      if vim.fn.isdirectory(path) ~= 0 == false then
+        path = vim.fn.fnamemodify(path, ":p:h")
+      end
+
+      require("fzf-lua").live_grep({ cwd = path })
+    end, { buffer = true })
+  end,
+})
+
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function ()
+    require("oil").setup({
+      use_default_keymaps = false,
+      keymaps = {
+        ["<CR>"] = "actions.select",
+        ["<leader><CR>"] = "actions.select_vsplit",
+        ["<leader>p"] = "actions.preview",
+        ["<C-r>"] = "actions.refresh",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+      buf_options = {
+        buflisted = true,
+        bufhidden = "hide",
+      },
+      lsp_rename_autosave = true,
+      skip_confirm_for_simple_edits = true,
+      prompt_save_on_select_new_entry = false,
+    })
+  end,
+}
