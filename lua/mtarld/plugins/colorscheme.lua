29a478d037f2fcfbf140f8497646383ff5404bc0
Mathias Arlaud <mathias.arlaud@gmail.com>
2024-10-08 17:20:31 +0200

    update theme

--- lua/mtarld/plugins/colorscheme.lua
+++ lua/mtarld/plugins/colorscheme.lua
@@ -5,19 +5,19 @@ return {
   opts = {},
   config = function()
     if "true" == os.getenv("DARK_THEME") then
-      vim.cmd.colorscheme("github_dark")
+      vim.cmd.colorscheme("github_dark_dimmed")
     else
-      vim.cmd.colorscheme("github_light")
+      vim.cmd.colorscheme("github_light_default")
     end
   end,
   keys = {
     {
       "<leader>tt",
       function()
-        if vim.g.colors_name == "github_light" then
-          vim.cmd.colorscheme("github_dark")
+        if vim.g.colors_name == "github_light_default" then
+          vim.cmd.colorscheme("github_dark_dimmed")
         else
-          vim.cmd.colorscheme("github_light")
+          vim.cmd.colorscheme("github_light_default")
         end
       end,
     },
