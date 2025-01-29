b097944ce65b20e3b441e18fa6db1b0d398562b5
Mathias Arlaud <mathias.arlaud@gmail.com>
2025-01-29 10:39:50 +0100

    add blink copilot

--- lua/mtarld/plugins/blink.lua
+++ lua/mtarld/plugins/blink.lua
@@ -18,6 +18,24 @@ return {
       return not vim.tbl_contains({ "laika" }, vim.bo.filetype)
     end,
     sources = {
+      default = { "copilot" },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
+          transform_items = function(_, items)
+            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+            local kind_idx = #CompletionItemKind + 1
+            CompletionItemKind[kind_idx] = "Copilot"
+            for _, item in ipairs(items) do
+              item.kind = kind_idx
+            end
+            return items
+          end,
+        },
+      },
       cmdline = function()
         local type = vim.fn.getcmdtype()
 
