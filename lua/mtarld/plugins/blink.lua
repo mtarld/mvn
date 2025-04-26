00f9bd4a2346b413f043d3647aaf4b8e880681a2
Mathias Arlaud <mathias.arlaud@gmail.com>
2025-04-26 12:24:19 +0200

    add better copilot integration

--- lua/mtarld/plugins/blink.lua
+++ lua/mtarld/plugins/blink.lua
@@ -2,9 +2,6 @@ return {
   'saghen/blink.cmp',
   version = 'v0.11.0',
   lazy = false, -- lazy loading handled internally
-  dependencies =  {
-    { "giuxtaposition/blink-cmp-copilot" }
-  },
   opts_extend = { "sources.default" },
   opts = {
     keymap = {
@@ -13,28 +10,15 @@ return {
       ['<C-k>'] = { 'select_prev', 'fallback' },
       ['<C-j>'] = { 'select_next', 'fallback' },
     },
-    enabled = function()
-      return not vim.tbl_contains({ "laika" }, vim.bo.filetype)
-    end,
-    sources = {
-      default = { "lsp", "path", "buffer", "copilot" },
-      providers = {
-        copilot = {
-          name = "copilot",
-          module = "blink-cmp-copilot",
-          score_offset = 100,
-          async = true,
-          transform_items = function(_, items)
-            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-            local kind_idx = #CompletionItemKind + 1
-            CompletionItemKind[kind_idx] = "Copilot"
-            for _, item in ipairs(items) do
-              item.kind = kind_idx
-            end
-            return items
-          end,
+    completion = {
+      list = {
+        selection = {
+          preselect = false,
+          auto_insert = true,
         },
       },
+    },
+    sources = {
       cmdline = function()
         local type = vim.fn.getcmdtype()
 
@@ -45,42 +29,5 @@ return {
         return {}
       end,
     },
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
-      kind_icons = {
-        Copilot = "",
-        Text = '󰉿',
-        Method = '󰊕',
-        Function = '󰊕',
-        Constructor = '󰒓',
-
-        Field = '󰜢',
-        Variable = '󰆦',
-        Property = '󰖷',
-
-        Class = '󱡠',
-        Interface = '󱡠',
-        Struct = '󱡠',
-        Module = '󰅩',
-
-        Unit = '󰪚',
-        Value = '󰦨',
-        Enum = '󰦨',
-        EnumMember = '󰦨',
-
-        Keyword = '󰻾',
-        Constant = '󰏿',
-
-        Snippet = '󱄽',
-        Color = '󰏘',
-        File = '󰈔',
-        Reference = '󰬲',
-        Folder = '󰉋',
-        Event = '󱐋',
-        Operator = '󰪚',
-        TypeParameter = '󰬛',
-      },
-    },
-  }
+  },
 }
--- lua/mtarld/plugins/copilot.lua
+++ lua/mtarld/plugins/copilot.lua
@@ -1,9 +1,88 @@
 return {
-  "zbirenbaum/copilot.lua",
-  event = "InsertEnter",
-  cmd = "Copilot",
-  opts = {
-    suggestion = { enabled = false },
-    panel = { enabled = false },
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    cmd = "Copilot",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    },
+  },
+  {
+    "robitx/gp.nvim",
+    config = function()
+      require("gp").setup({
+        default_command_agent = "Copilot",
+        default_chat_agent = "Copilot",
+        providers = {
+          openai = {
+            disable = true,
+          },
+          copilot = {
+            endpoint = "https://api.githubcopilot.com/chat/completions",
+            secret = {
+              "bash",
+              "-c",
+              "cat ~/.config/github-copilot/apps.json | sed -e 's/.*oauth_token...//;s/\".*//'",
+            },
+          },
+        },
+        agents = {
+          {
+            provider = "copilot",
+            name = "Copilot",
+            chat = true,
+            command = true,
+            model = { model = "gpt-4o" },
+            system_prompt = [[
+Respond to user messages according to the following principles:
+- Do not repeat the user's request and return only the response to the user's request.
+- Unless otherwise specified, respond in the same language as used in the user's request.
+- Be as accurate as possible.
+- Be as truthful as possible.
+- Be as comprehensive and informative as possible.
+- Do not give any explanation unless explicitly asked for.
+- In PHP use PHP 8 attributes and functionalities if possible (match, fn)
+- Do not add comments explaining what you did, I understand the code
+- You can add doc-block comments on function arguments if needed
+- Do not change the given code even if the logic looks wrong, in that case just add a comment
+            ]],
+          },
+        },
+        hooks = {
+          Implement = function(gp, params)
+            local template = "Having following from {{filename}}:\n\n"
+              .. "```{{filetype}}\n{{selection}}\n```\n\n"
+              .. "Please rewrite this according to the contained instructions."
+              .. "\n\nRespond exclusively with the snippet that should replace the selection above."
+
+            local agent = gp.get_command_agent()
+
+            gp.Prompt(params, gp.Target.rewrite, agent, template)
+          end,
+          CodeReview = function(gp, params)
+            local template = "I have the following code from {{filename}}:\n\n"
+              .. "```{{filetype}}\n{{selection}}\n```\n\n"
+              .. "Please analyze for code smells and suggest improvements."
+            local agent = gp.get_chat_agent()
+            gp.Prompt(params, gp.Target.vnew("markdown"), agent, template)
+          end,
+          Explain = function(gp, params)
+            local template = "I have the following code from {{filename}}:\n\n"
+              .. "```{{filetype}}\n{{selection}}\n```\n\n"
+              .. "Please respond by explaining the code above."
+            local agent = gp.get_chat_agent()
+            gp.Prompt(params, gp.Target.popup, agent, template)
+          end,
+        },
+      })
+    end,
+    cmd = { "GpCodeReview", "GpExplain", "GpContext" },
+    keys = {
+      { "<leader>aa", ":GpChatToggle vsplit<CR>", mode = {"n", "i"} },
+      { "<leader>aa", ":<C-u>'<,'>GpChatPaste vsplit<CR>", mode = "v" },
+      { "<leader>ai", ":<C-u>'<,'>GpImplement<CR>", mode = "v" },
+      { "<leader>ar", ":<C-u>'<,'>GpRewrite<CR>", mode = "v" },
+    },
   },
 }
